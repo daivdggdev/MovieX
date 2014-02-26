9c00160772847663b8136f778586416c0909e1db
daivdggdev <daivdggdev@163.com>
2014-02-26 13:54:03 +0800

    +): delete Podfile.lock

---
+++ PPStreamX/XHomeToolsViewController.h
@@ -0,0 +1,13 @@
+//
+//  XHomeToolsViewController.h
+//  PPStreamX
+//
+//  Created by 王大伟 on 14-2-25.
+//  Copyright (c) 2014年 pps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHomeToolsViewController : UIViewController
+
+@end
---
+++ PPStreamX/XHomeToolsViewController.m
@@ -0,0 +1,38 @@
+//
+//  XHomeToolsViewController.m
+//  PPStreamX
+//
+//  Created by 王大伟 on 14-2-25.
+//  Copyright (c) 2014年 pps. All rights reserved.
+//
+
+#import "XHomeToolsViewController.h"
+
+@interface XHomeToolsViewController ()
+
+@end
+
+@implementation XHomeToolsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
